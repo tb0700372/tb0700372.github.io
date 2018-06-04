e9b81407cf3f59d72df81101998f416b315ab451
大龙 <763221847@qq.com>
2018-06-04 23:11:59 +0800

    Add files via upload

---
+++ my.js
@@ -0,0 +1,7 @@
+$("#list img").mouseover(function() {
+    $("#mid").attr("src", $(this).attr("src"));
+});
+$(".p-img").mouseover(function() { $(".main-img").attr("src", $(this).attr("src")); });
+$(".p-count-btn").click(function() {
+	
+});
